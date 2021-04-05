b410cc8ad85e80aa8b32590c980db0ad9067c1c7
Sherry <Shahryartariq57@gmail.com>
2021-04-06 02:30:42 +0500

    second commit

--- src/Card.jsx
+++ src/Card.jsx
@@ -8,7 +8,7 @@ const Card = (props) => {
     <>
       <div className="col-md-4 col-10 mx-auto">
         <div className="card">
-          <img src={props.imgsrc} height="200px" className="card-img-top" alt="cardImage" />
+          <img src={props.imgsrc} height="280px" className="card-img-top" alt="cardImage" />
           <div className="card-body">
             <h5 className="card-title">{props.title}</h5>
             <p className="card-text">
